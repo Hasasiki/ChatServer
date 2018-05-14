5f93276b58345cc51e918658b8f3326214b39858
Hasasiki <1243442778@qq.com>
2018-05-14 19:20:41 +0800

    The single thread server

--- src/OS_Project/SocketServer.java
+++ src/OS_Project/SocketServer.java
@@ -21,10 +21,57 @@ public class SocketServer {
 		try {
 			ServerSocket server = null;
 			try {
-				server = new ServerSocket(5209);
+				server = new ServerSocket(5303);
 				//指定绑定的端口并监听此端口
+				System.out.println("Server start!");
+				//
+			}catch(Exception e) {
+				System.out.println("hasn't start monitor"+e);//打印错误信息
 			}
+			
+			Socket socket = null;
+			try {
+				socket = server.accept();
+				//调用accept方法监听，等待客户链接，使用accept方法阻塞等待客户请求，有客户请求到来时则产生一个socket对象并继续执行
+				//单线程处理，一次只允许一个客户端访问
+				//THERE NEED A Supporting Multiple Clients!
+				/*
+				 while (true) {
+    					accept a connection;
+    					create a thread to deal with the client;
+				 }
+				 */
+				}catch(Exception e) {
+					System.out.println("Error."+e);//show error message
+				}
+			//获取输入流，读取客户端消息
+			String line;
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			//由Socket对象得到输入流，并构造相应的BufferedReader对象
+			PrintWriter writer = new PrintWriter(socket.getOutputStream());
+			//由socket对象得到输出流，并构造PrintWriter对象
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			//由标准输出上打印从客户端读入的字符串
+			System.out.println("Client:" + in.readLine());
+			line = br.readLine();
+			//从标准输入读入一字符串
+			
+			// -->获取输入流，相应客户端请求
+			while(!line.equals("end")) {//如果字符串为“end”，则停止循环
+				writer.println(line);//向客户端输出该字符串
+				writer.flush();//刷新输入流，使客户端马上收到该字符串
+				System.out.println("Server:"+line);//在系统输出上打印读入的字符串
+				System.out.println("Client:"+in.readLine());//从Client读入一字符串，并打印到输出
+				line = br.readLine();//从系统标准输入读入字符串
+			}
+			//关闭资源
+			writer.close();//关闭socket输出流
+			in.close();//关闭socket输入流
+			socket.close();//关闭socket
+			server.close();//关闭serverSocket
+		}catch(Exception e) {
+			System.out.println("Error."+e);
 		}
 	}
-
 }
+//course resource copy by https://www.cnblogs.com/zhoudi/p/6025552.html
